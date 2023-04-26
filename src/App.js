6bd00b3e7850290af529cb8e277354009deadc98
Leandro Brangi <leandro.brangi@gmail.com>
2023-04-25 21:21:53 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { Home, Detail, Landing, Create } from "./views/index.js";
 import NavBar from "./Components/NavBar/NavBar"
 import Footer from "./Components/Footer/Footer.jsx";
 import { Route, useLocation } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://countries-back-production-a139.up.railway.app/' 
+
 
 function App() {
   const location = useLocation();
+
+  axios.defaults.baseURL = 'https://countries-back-production-a139.up.railway.app/' 
 
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import axios from 'axios';
 import App from "./App";
 
 // axios.defaults.baseURL = 'http://localhost:3001' 
-axios.defaults.baseURL = 'https://countries-back-production-a139.up.railway.app/' 
+
 
 
 ReactDOM.render(
